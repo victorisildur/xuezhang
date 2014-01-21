502f55c2c5f7574a43ecebac9201e7c499fe4bfa
c00lyu <coolyugogogo@gmail.com>
2014-01-21 20:07:43 +0800

    微信API代码更新
    实现了发送被动响应消息接口，未debug
    待实现接收消息接口、发送客服消息接口

---
+++ SourceCode/weixinAPI/weixin_class.php
@@ -0,0 +1,129 @@
+<?php
+//装载模板文件
+include_once("weixin_tpl.php");
+
+//自定义微信信息处理类
+class WeiXinAPI
+{
+	private $postObj;
+	private $fromUsername;
+	private $toUsername;
+	public $form_MsgType;
+	
+	//NEWS相关
+	private $newsCount = 0;
+	private $newsBody = '';
+	
+	
+	//把post来的数据进行处理
+	public function parsePostStr()
+	{
+		//获取微信发送数据
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+		//返回回复数据
+		if (!empty($postStr))
+		{  
+			//解析数据
+			  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+			//发送消息方ID
+			  $fromUsername = $postObj->FromUserName;
+			//接收消息方ID
+			  $toUsername = $postObj->ToUserName;
+			//消息类型
+			  $form_MsgType = $postObj->MsgType;
+			  
+			  return true;
+		}
+		else 
+		{
+			  return false;
+		}
+	}
+/****************************
+各种类型消息返回信息生成开始
+*****************************/
+	//纯文本消息
+	public function genTextMsg($contentStr)
+	{
+		return sprintf(WEIXIN_TEXT_MSG, this->$fromUsername, this->$toUsername, time(), $contentStr);
+	}
+	//图文消息
+	//$url点击图文消息跳转链接
+	//$picUrl图片链接，支持JPG、PNG格式，较好的效果为大图360*200，小图200*200
+	public function addArticle($title, $description, $picUrl, $url)
+	{
+		if(this->$newsCount > 10) return false;
+		$newsBody += sprintf(WEIXIN_NEWS_MSG_BODY, $title, $description, $picUrl, $url);
+		this->$newsCount += 1;
+		return true;
+	}
+	public function addArticleFinish()
+	{
+		this->$newsBody = sprintf(WEIXIN_NEWS_MSG_BEGIN, this->$fromUsername, this->$toUsername, time(), this->$newsCount) + this->$newsBody;
+		this->$newsBody += WEIXIN_NEWS_MSG_END;
+		return this->$newsBody;
+	}
+	//图片消息
+	//$mediaId通过上传多媒体文件，得到的id
+	public function genImageMsg($mediaId)
+	{
+		return sprintf(WEIXIN_IMAGE_MSG, this->$fromUsername, this->$toUsername, time(), $mediaId);
+	}
+	//语音消息
+	//$mediaId通过上传多媒体文件，得到的id
+	public function genVoiceMsg($mediaId)
+	{
+		return sprintf(WEIXIN_VOICE_MSG, this->$fromUsername, this->$toUsername, time(), $mediaId);
+	}
+	//视频消息
+	//$mediaId通过上传多媒体文件，得到的id，必须
+	//其余项非必须
+	public function genVideoMsg($mediaId, $title = '', $description = '')
+	{
+		return sprintf(WEIXIN_VIDEO_MSG, this->$fromUsername, this->$toUsername, time(), $mediaId, $title, $description);
+	}
+	//音乐消息
+	//$mediaId缩略图的媒体id，通过上传多媒体文件，得到的id
+	//$mediaId为必须项，其他项非必须
+	//$HQMusicUrl高质量音乐链接，WIFI环境优先使用该链接播放音乐
+	public function genVoiceMsg($title = '', $description = '', $musicUrl = '', $HQMusicUrl = '', $mediaId)
+	{
+		return sprintf(WEIXIN_MUSIC_MSG, this->$fromUsername, this->$toUsername, time(), $title, $description, $musicUrl, $HQMusicUrl, $mediaId);
+	}
+/****************************
+各种类型消息返回信息生成结束
+*****************************/	
+	
+	
+	//验证签名是否合法，用于最初的网址接入
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if(this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
+    }
+	private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = WEIXIN_TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
---
+++ SourceCode/weixinAPI/weixin_config.php
@@ -0,0 +1,4 @@
+<?php
+define("WEIXIN_TOKEN", "weixin");
+define("WEIXIN_APPID", "weixin");
+define("WEIXIN_APPSECRET", "weixin");
