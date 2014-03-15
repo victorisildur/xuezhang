3131d574c8ec97e765fa5909263b0210984554d5
victorisildur <victorisildur@163.com>
2014-03-15 14:07:41 +0800

    改版0.1
    ss

---
+++ SourceCode/frontend/index.php
@@ -0,0 +1,9 @@
+$goods_new_imgs_li .= "<li>
+						<a href= $url > <img src= $img_url>"/> </a>
+					</li>"
+					
+$goods_top_li .= "<li>
+						<a href= $url > <img src= $img_url>"/> </a>
+						已售：<span text = $sale_num> </span>
+						售价：<span text = $price> </span>
+					</li>"
