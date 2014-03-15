45de4f5d6134422fe19c23e88b309ed5a94cc915
victorisildur <victorisildur@163.com>
2014-03-15 14:28:50 +0800

    改版0.11
    detail

---
+++ SourceCode/frontend/detail.php
@@ -0,0 +1,11 @@
+$price
+$sale_num
+$purchase_a_button = "<a href = $purchase_url><button>购买</button></a>"
+$description_html = "<p> 特别说明：$description</p>"
+
+while 
+$detail_imgs .= "<li>
+						<a href= $url > <img src= $img_url>"/> </a>
+						已售：<span text = $sale_num> </span>
+						售价：<span text = $price> </span>
+					</li>"
