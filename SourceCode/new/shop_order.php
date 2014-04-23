41498b914ccc06df69e41034134b4ef1f41fdcc9
victorisildur <victorisildur@163.com>
2014-04-23 22:48:50 +0800

    shop_order逻辑补全
    js化

--- SourceCode/new/shop_order.php
+++ SourceCode/new/shop_order.php
@@ -25,10 +25,42 @@ foreach($data as $result)
 $tmp['did_array'] = $data;
 $json = json_encode($tmp);
 
-foreach( $json['did_array'] as $did_entry )
+/*foreach( $tmp['did_array'] as $did_entry )
 {
-	$did_array[] = $did_entry['detail_id];
-	echo $did_array[];
-}
-
+	$did_array[] = $did_entry['detail_id'];
+	echo $did_array;
+}*/
+/*output <label/> and <input/> for sex field*/
+$labelInputForSexMale = '<label for="male">男
+							
+							<input type="radio" data-bind="checked: sex" name="sex" value="1" id="male">
+						</label>';
+$labelInputForSexFemale = '<label for="female">女
+							
+							<input type="radio" data-bind="checked: sex" name="sex" value="0" id="female">
+							
+						</label>';
+/*output <label/> and <input/> for color field*/
+$labelInputForColorWhite = ' <label for="white">白色
+						<input type="radio" data-bind="checked: color" name="color" value="0" id="white"/> 
+						</label>';
+$labelInputForColorGrey =  ' <label for="gray">灰色
+						<input type="radio" data-bind="checked: color" name="color" value="2" id="gray"/> 
+						</label>';
+$labelInputForColorBlack = ' <label for="black">黑色
+						<input type="radio" data-bind="checked: color" name="color" value="1" id="black"/>
+						</label>' ;
+/*output <label/> and <input/> for size field*/
+$labelInputForSizeM = ' <label for="m">M
+						<input type="radio" data-bind="checked: size" name="size" id="m" value="1"> 
+						</label>';
+$labelInputForSizeL = '<label for="l">L
+						<input type="radio" data-bind="checked: size" name="size" id="l" value="2">
+						</label>';
+$labelInputForSizeXL = '<label for="xl">XL
+						<input type="radio" data-bind="checked: size" name="size" id="xl" value="3">
+						</label>';
+$labelInputForSizeXXL = '<label for="xxl">XXL
+						<input type="radio" data-bind="checked: size" name="size" id="xxl" value="4">
+						</label>';
 require_once(TEMPLATES_PATH.'shop_order.html');
---
+++ SourceCode/new/static/js/purchase2.js
@@ -0,0 +1,150 @@
+﻿$(document).ready( function() {
+	
+});
+
+
+ 
+
+function OrderConstructor(data){
+	
+	this.num = ko.observable(data.num);
+	this.detail_id = data.detail_id;
+	this.color = ko.observable(data.color);
+	this.size = ko.observable(data.size);
+}
+
+
+function OrderViewModel() {
+	var detailIdJson = eval("("+shop_goods_detail+")");
+	var didArray = detailIdJson.did_array;
+	var self = this;
+	self.orderFull = ko.observableArray([]);
+	
+	//color,size,time,sex
+	self.color = ko.observable();
+	self.size = ko.observable();
+	self.time = ko.observable();
+	self.sex = ko.observable();
+	//left
+	self.left = ko.computed( function() {
+		var i = findIndexInDidArrayMatchingSelection();
+		console.log("index:"+i);
+		if(i!= -1 )
+		{
+			var left = didArray[i].left;
+			return left;
+		} else {
+			return 0;
+		}
+	});
+	
+	//输入的name,phone,addr,num值
+	self.newNameText = ko.observable();
+	self.newPhoneText = ko.observable();
+	self.newAddrText = ko.observable();
+	self.newNumText = ko.observable();
+	//加一单按钮文本
+	self.addOrderBtnText = ko.observable("预览订单");
+	
+	//加一单
+	self.addOrder = function(){
+		console.log("press add order btn");
+		console.log("select color:"+this.color() );
+		this.addOrderBtnText("加一单");
+		
+		if(isPhone(this.newPhoneText()) && isChinese(this.newNameText()) ) {
+			console.log("select color in if:"+this.color() );
+			
+			var i = findIndexInDidArrayMatchingSelection();
+			if(i== -1 )
+			{
+				alert("没有这款库存！");
+			} else {
+				var selectedDetailId = detailIdJson.did_array[i].detail_id;
+				console.log("detail_id matched!! value is :" + selectedDetailId);
+			}
+			
+			var orderItem = {
+							detail_id:selectedDetailId,
+							num: Number(this.newNumText()),
+							color: this.color(),
+							size:  this.size()
+							};
+			self.orderFull.push(new OrderConstructor(orderItem));
+		}
+		else {
+			alert("手机号码或姓名输入不正确");
+		}
+	}
+	
+	//删除订单
+	self.deleteOrder = function(orderItem) {
+		self.orderFull.remove(orderItem);
+	}
+	
+	//提交订单
+	self.submitOrder = function() {
+		console.log("press submit order btn");
+		if(isPhone(this.newPhoneText()) && isChinese(this.newNameText()) ) {
+			//send to shop_takeorder.php
+			var mydata = { "real_name": this.newNameText,
+						   "phone": this.newPhoneText,
+						   "address" : this.newAddrText,
+						   "message" : this.newMsgTest,
+						   "best_time" : this.time(),
+						   "orders": ko.toJS(self.orderFull)
+							  };
+			$.post("http://localhost/shop_takeorder.php",
+				mydata, function(data) {
+					
+				},"json"
+			);
+		}
+    };
+	
+	//判断手机号
+	function isPhone(inputString)
+    {
+	    var partten = /^1[3,5,8]\d{9}$/;
+	    var fl=false;
+	    if(partten.test(inputString))
+	    {
+	        console.log('是手机号码');
+	        return true;
+	    }
+	    else
+	    {
+			console.log('不是手机号码');
+	        return false;
+	    }
+    }
+	
+	//判断汉字
+	function isChinese(inputString) {
+		var pattern = new RegExp("[\\u4E00-\\u9FFF]","g");
+		return pattern.test(inputString);
+	}
+	
+	//return index in did_array that matches 'sex','color','size' that we selected
+	//if no one matches, return -1
+	function findIndexInDidArrayMatchingSelection() {
+	
+	var i=0;
+	while(i<detailIdJson.did_array.length) {
+		if(detailIdJson.did_array[i].color==self.color() &&
+		   detailIdJson.did_array[i].size ==self.size()  &&
+		   detailIdJson.did_array[i].sex  ==self.sex() )
+			break;
+		i++;
+	}
+	if(i==detailIdJson.did_array.length)
+	{
+		return -1;
+	} else {
+		return i;
+	}
+	
+}
+}
+
+ko.applyBindings(new OrderViewModel());
