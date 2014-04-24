c21fc1bc7c1b414bc3570a6ecaf586641b5f5877
victorisildur <victorisildur@163.com>
2014-04-24 16:03:39 +0800

    order修改
    shop_order

--- SourceCode/new/shop_order.php
+++ SourceCode/new/shop_order.php
@@ -31,36 +31,42 @@ $json = json_encode($tmp);
 	echo $did_array;
 }*/
 /*output <label/> and <input/> for sex field*/
-$labelInputForSexMale = '<label for="male">男
-							
+$labelInputForSexMale = '<label for="male">
 							<input type="radio" data-bind="checked: sex" name="sex" value="1" id="male">
+							<span class="check"></span>男
 						</label>';
-$labelInputForSexFemale = '<label for="female">女
-							
+$labelInputForSexFemale = '<label for="female">
 							<input type="radio" data-bind="checked: sex" name="sex" value="0" id="female">
-							
+							<span class="check"></span>女
 						</label>';
 /*output <label/> and <input/> for color field*/
-$labelInputForColorWhite = ' <label for="white">白色
+$labelInputForColorWhite = ' <label for="white">
 						<input type="radio" data-bind="checked: color" name="color" value="0" id="white"/> 
+						<span class="check"></span>白色
 						</label>';
-$labelInputForColorGrey =  ' <label for="gray">灰色
+$labelInputForColorGrey =  ' <label for="gray">
 						<input type="radio" data-bind="checked: color" name="color" value="2" id="gray"/> 
+						<span class="check"></span>灰色
 						</label>';
-$labelInputForColorBlack = ' <label for="black">黑色
+$labelInputForColorBlack = ' <label for="black">
 						<input type="radio" data-bind="checked: color" name="color" value="1" id="black"/>
+						<span class="check"></span>黑色
 						</label>' ;
 /*output <label/> and <input/> for size field*/
-$labelInputForSizeM = ' <label for="m">M
+$labelInputForSizeM = ' <label for="m">
 						<input type="radio" data-bind="checked: size" name="size" id="m" value="1"> 
+						<span class="check"></span>M
 						</label>';
-$labelInputForSizeL = '<label for="l">L
+$labelInputForSizeL = '<label for="l">
 						<input type="radio" data-bind="checked: size" name="size" id="l" value="2">
+						<span class="check"></span>L
 						</label>';
-$labelInputForSizeXL = '<label for="xl">XL
+$labelInputForSizeXL = '<label for="xl">
 						<input type="radio" data-bind="checked: size" name="size" id="xl" value="3">
+						<span class="check"></span>XL
 						</label>';
-$labelInputForSizeXXL = '<label for="xxl">XXL
+$labelInputForSizeXXL = '<label for="xxl">
 						<input type="radio" data-bind="checked: size" name="size" id="xxl" value="4">
+						<span class="check"></span>XXL
 						</label>';
 require_once(TEMPLATES_PATH.'shop_order.html');
